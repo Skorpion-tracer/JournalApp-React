60dc64a16e72d8a44500d377562fcf5a5b56d330
Vitaliy <v.brazhnik@it-ansoft.ru>
2026-09-07 23:53:03 +0300

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import JournalAddButton from './components/JournalAddButton/JournalAddButton.jsx
 import JournalForm from './components/JournalForm/JournalForm.jsx';
 import {useLocalstorage} from './hooks/use-localstorage.hook.js';
 import {UserContextProvider} from './context/user.context.jsx';
-import {useCallback, useState} from 'react';
+import {useState} from 'react';
 
 function mapItems(items) {
     if (!items) {
